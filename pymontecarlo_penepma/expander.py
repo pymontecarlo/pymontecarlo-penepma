428ae1ec1aedd751a44d4d1ac7f7a8833deb0826
Philippe Pinard <philippe.pinard@oxinst.com>
2019-05-17 15:59:34 +0100

    Remove unused methods

--- pymontecarlo_penepma/expander.py
+++ pymontecarlo_penepma/expander.py
@@ -11,15 +11,5 @@ from pymontecarlo.options.program.expander import ExpanderBase, expand_to_single
 
 class PenepmaExpander(ExpanderBase):
 
-    def expand_detectors(self, detectors):
-        # TODO: Fix
-        return expand_to_single(detectors)
-
-    def expand_limits(self, limits):
-        return expand_to_single(limits)
-
-    def expand_models(self, models):
-        return expand_to_single(models)
-
     def expand_analyses(self, analyses):
         return expand_to_single(analyses)
